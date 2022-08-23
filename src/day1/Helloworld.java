cb2159129ccff0dc03a70560f2d9f307c2810ff0
hadda <haddad.dolly@yahoo.com>
2022-08-22 22:53:10 -0400

    HelloWorld.java is updated again

--- src/day1/Helloworld.java
+++ src/day1/Helloworld.java
@@ -4,6 +4,7 @@ public class Helloworld {
     public static void main(String[] args) {
         System.out.println("good old days");
 
-        //some work here
+        //some work here to commit
+
     }
 }
